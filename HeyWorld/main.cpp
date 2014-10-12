142c667c2e239d998b7250be58c11f78d9907248
Montse Lozano Dieck <montse_967@hotmail.com>
2014-10-12 16:16:14 -0500

    First Commit

---
+++ HeyWorld/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HeyWorld
+//
+//  Created by Maria Montserrat Lozano on 12/10/14.
+//  Copyright (c) 2014 Montserrat Lozano Dieck. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
