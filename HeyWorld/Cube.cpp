e2b6c59cca77a679b2f300bd8b473e7da0405f0c
Montse Lozano Dieck <montse_967@hotmail.com>
2014-11-15 14:51:38 -0600

    agregar clases del cubo

---
+++ HeyWorld/Cube.cpp
@@ -0,0 +1,82 @@
+//
+//  Cube.cpp
+//  LightsBox
+//
+//  Created by Maria Montserrat Lozano on 15/11/14.
+//  Copyright (c) 2014 Montserrat Lozano Dieck. All rights reserved.
+//
+
+#include "Cube.h"
+
+Cube::Cube()
+{
+
+}
+
+void Cube::drawCube()
+{
+    //toda la caja
+    glPushMatrix();
+    glRotated(angleBox, 0, 1, 0);
+    //parta baja
+    glPushMatrix();
+    // glColor3f(0.0, 1.0, 0.0);
+    // glutSolidCube(1.0);
+    glColor3f(1.0, 1.0, 1.0);
+    glutWireCube(1.0);
+    glPopMatrix();
+    
+    //parte para abrir
+    glPushMatrix();
+    glRotated(angleTop, 1, 0, 0);
+    glTranslated(0, 0.6, 0);
+    glScalef(1.0, 0.25, 1.0);
+    glColor3f(0.0, 1.00, 0);
+    glutSolidCube(1.0);
+    glColor3f(1.0, 1.0, 1.0);
+    glutWireCube(1.0);
+    glPopMatrix();
+    
+    glPopMatrix();
+}
+
+void Cube::animationBox(int v)
+{
+    if(v == 0) //abrir tapa
+    {
+        if(angleTop > 0)
+        {
+            angleTop--;
+            glutTimerFunc(50, animationBox, 0);
+            
+        }
+        else
+            angleTop = 0;
+    }else if (v == 1) //rotar caja
+    {
+        if(angleBox < 360)
+        {
+            angleBox++;
+            glutTimerFunc(50, animationBox, 1);
+        }
+        else
+            angleBox = 0;
+    }
+    glutPostRedisplay();
+}
+
+void Cube::openCube()
+{
+    glutTimerFunc(50, animationBox, 0);
+}
+
+void Cube::closeCube()
+{
+    angleTop = 90;
+    glutPostRedisplay();
+}
+
+void Cube::rotateCube()
+{
+     glutTimerFunc(100, animationBox, 1);
+}
---
+++ HeyWorld/Cube.h
@@ -0,0 +1,30 @@
+//
+//  Cube.h
+//  LightsBox
+//
+//  Created by Maria Montserrat Lozano on 15/11/14.
+//  Copyright (c) 2014 Montserrat Lozano Dieck. All rights reserved.
+//
+
+#ifndef __LightsBox__Cube__
+#define __LightsBox__Cube__
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <stdio.h>
+static int angleBox = 0;
+static int angleTop = 90;
+class Cube
+{
+private:
+    static void animationBox(int v);
+public:
+    Cube();
+    void drawCube();
+    void openCube();
+    void closeCube();
+    void rotateCube();
+};
+#endif /* defined(__LightsBox__Cube__) */
